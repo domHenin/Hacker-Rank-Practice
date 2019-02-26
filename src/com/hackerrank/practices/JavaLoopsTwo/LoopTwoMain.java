7bf36f967960990304acff5adf33042d17ce15c4
domHenin <domhenin.517@gmail.com>
2019-02-25 23:14:55 -0500

    check out

---
+++ src/com/hackerrank/practices/JavaLoopsTwo/LoopTwoMain.java
@@ -0,0 +1,7 @@
+package com.hackerrank.practices.JavaLoopsTwo;
+
+public class LoopTwoMain {
+    public static void main(String[] args) {
+        System.out.println("to GOD all glory!");
+    }
+}
