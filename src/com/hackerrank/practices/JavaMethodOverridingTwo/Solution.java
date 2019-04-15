911f693aa41284d1b2832b8fda022e9a145b4ea5
domHenin <domhenin.517@gmail.com>
2019-04-15 09:57:08 -0400

    challenge complete

--- src/com/hackerrank/practices/JavaMethodOverridingTwo/Solution.java
+++ src/com/hackerrank/practices/JavaMethodOverridingTwo/Solution.java
@@ -19,10 +19,10 @@ class MotoCycle extends BiCycle {
     MotoCycle() {
         System.out.println("Hello I am a motorcycle, I am " +define_me());
 
-        String temp=define_me(); //Fix this line
+        String temp=super.define_me(); //Fix this line
 
 
-        System.out.println("My ancestor is a cycle who is "+super.define_me());
+        System.out.println("My ancestor is a cycle who is "+temp);  //super.define_me()
     }
 }
 
